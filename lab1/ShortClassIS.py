dc20c58f6349b5f7574e4a5ab2c08bfb941f3813
ryglo1337 <115521236+ryglo1337@users.noreply.github.com>
2024-12-18 22:56:24 +0300

    Delete lab1 directory

--- lab1/ShortClassIS.py
+++
@@ -1,6 +0,0 @@
-class ShortClient(Client):
-    def full_info(self):
-        raise NotImplementedError("Полная информация недоступна для краткой версии.")
-    
-    def short_info(self):
-        return f"{self.last_name} {self.first_name[0]}."
--- lab1/delRepIS.py
+++
@@ -1,7 +0,0 @@
-  @staticmethod
-    def validate_string(value: str, field_name: str, max_length: int = 255):
-        if not value or not isinstance(value, str):
-            raise ValueError(f"{field_name} должно быть строкой.")
-        if len(value) > max_length:
-            raise ValueError(f"{field_name} не может быть длиннее {max_length} символов.")
-        return value
